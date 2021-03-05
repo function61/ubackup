a3677c5e9bfe257312ae43a9bb5b0be2b9dc081a
Joonas Loppi <joonas@joonas.fi>
2021-03-05 09:11:46 +0200

    storage CLI: clean up code

--- cmd/ubackup/storage.go
+++ cmd/ubackup/storage.go
@@ -13,70 +13,66 @@ import (
 )
 
 func storageEntry() *cobra.Command {
-	ls := func(serviceId string) error {
-		conf, err := ubconfig.ReadFromEnvOrFile()
-		if err != nil {
-			return err
-		}
-
-		storage, err := ubstorage.StorageFromConfig(conf.Storage, logex.StandardLogger())
-		if err != nil {
-			return err
-		}
-
-		backups, err := storage.List(serviceId)
-		if err != nil {
-			return err
-		}
-
-		for _, backup := range backups {
-			fmt.Println(backup.ID)
-		}
-
-		return nil
-	}
-
-	get := func(id string) error {
-		conf, err := ubconfig.ReadFromEnvOrFile()
-		if err != nil {
-			return err
-		}
-
-		storage, err := ubstorage.StorageFromConfig(conf.Storage, logex.StandardLogger())
-		if err != nil {
-			return err
-		}
-
-		body, err := storage.Get(id)
-		if err != nil {
-			return err
-		}
-		defer body.Close()
-
-		_, err = io.Copy(os.Stdout, body)
-		return err
-	}
-
 	cmd := &cobra.Command{
 		Use:   "storage",
 		Short: "Storage related commands",
 	}
 
 	cmd.AddCommand(&cobra.Command{
-		Use:   "ls [serviceId]",
-		Short: "List backups from storage for a service",
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			osutil.ExitIfError(ls(args[0]))
-		},
-	})
-
-	cmd.AddCommand(&cobra.Command{
 		Use:   "get [id]",
 		Short: "Get backup from storage",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			osutil.ExitIfError(get(args[0]))
+			osutil.ExitIfError(func(id string) error {
+				conf, err := ubconfig.ReadFromEnvOrFile()
+				if err != nil {
+					return err
+				}
+
+				storage, err := ubstorage.StorageFromConfig(conf.Storage, logex.StandardLogger())
+				if err != nil {
+					return err
+				}
+
+				body, err := storage.Get(id)
+				if err != nil {
+					return err
+				}
+				defer body.Close()
+
+				_, err = io.Copy(os.Stdout, body)
+				return err
+			}(args[0]))
+		},
+	})
+
+	cmd.AddCommand(&cobra.Command{
+		Use:   "ls [serviceId]",
+		Short: "List backups from storage for a service",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			osutil.ExitIfError(func(serviceId string) error {
+				conf, err := ubconfig.ReadFromEnvOrFile()
+				if err != nil {
+					return err
+				}
+
+				storage, err := ubstorage.StorageFromConfig(conf.Storage, logex.StandardLogger())
+				if err != nil {
+					return err
+				}
+
+				backups, err := storage.List(serviceId)
+				if err != nil {
+					return err
+				}
+
+				for _, backup := range backups {
+					fmt.Println(backup.ID)
+				}
+
+				return nil
+			}(args[0]))
 		},
 	})
 
